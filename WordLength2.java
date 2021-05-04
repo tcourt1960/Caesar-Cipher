7f4026aede17c1acb6337ebe7011b3ab2bd6dd31
krishna prasad peram <peramkrishnaprasad@gmail.com>
2021-05-04 17:08:35 +0530

    add your name
    add your name in comments section so that others know that this code belongs to you:)

--- WordLength2.java
+++ WordLength2.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of WordLength2 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author tcourt1960
+ * @version 2017
  */
 
 import edu.duke.*;
